39c2e67f1728cf912c90ebf46e555446f621fa7a
beaomoth <churmj@gmail.com>
2015-01-03 17:01:55 +0000

    Fix on com test
    String rather than numbers being sent

--- test_sketch/test_sketch.ino
+++ test_sketch/test_sketch.ino
@@ -3,18 +3,20 @@ int transmit_output_pin = 7;
 int transmit_input_pin = 6;
 
 SoftwareSerial transmitSerial(transmit_input_pin, transmit_output_pin);
-transmit_baud = 9600;
+int transmit_baud = 9600;
+int counter = 0;
 
 void setup(){
   transmitSerial.begin(transmit_baud);
 }
 
 void loop(){
-  while(1) {
-    counter = counter + 1;
-    if(counter == 10) {
-      counter = 0;
-      transmitSerial.print('It works!');
-    }
-  } 
+ // while(1) {
+  //  counter = counter + 1;
+   // if(counter == 10) {
+   //   counter = 0;
+      transmitSerial.print("Hello World \n\r");
+      delay(1000);
+    //}
+  //} 
 }  
